9edcc461bb6f4d8135564df06d676ddd1295118d
SaraLin3 <111754481+SaraLin3@users.noreply.github.com>
2023-11-24 13:27:01 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -11,8 +11,6 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objs as go
-from dash import dcc
-from dash import html
 # 
 data = pd.read_excel(r"D:\LJX\工作\4-东方固收\20223-10-10 GCI\副本各相关性指标和流动性.xlsx",index_col=0)
 data.replace(0, np.nan, inplace=True)
